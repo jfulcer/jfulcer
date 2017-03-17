11a6db3a8d6340477073d7e993805615ef9a98ea
jfulcer <jfulcer@uw.edu>
2017-03-16 19:02:59 -0700

    Add files via upload
    stuart's update

--- AppointmentReminders.Web/App_Start/Hangfire.cs
+++ AppointmentReminders.Web/App_Start/Hangfire.cs
@@ -9,15 +9,20 @@ namespace AppointmentReminders.Web
     {
         public static void ConfigureHangfire(IAppBuilder app)
         {
+            // commented out to test on Stuart's computer (transferring project and database connection issues)
+            /*
             GlobalConfiguration.Configuration.UseSqlServerStorage("DefaultConnection");
 
             app.UseHangfireDashboard("/jobs");
             app.UseHangfireServer();
+            */
         }
 
         public static void InitializeJobs()
         {
+            /*
             RecurringJob.AddOrUpdate<Workers.SendNotificationsJob>(job => job.Execute(), Cron.Minutely);
+            */
         }
     }
 }
